46d119ed00a2acf6f4063d33255815555a030a3e
Parv Garg <parvg764@gmail.com>
2023-09-11 06:59:45 +0530

    some changes

--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -40,19 +40,33 @@ const RoomController = {
 
   joinRoom: async (req, res) => {
     const { roomId } = req.body;
-  
+
     try {
+      // Verify the Firebase ID token to check user authentication
+      const idToken = req.headers.authorization;
+      if (!idToken) {
+        return res.status(401).json({ error: 'You must be logged in to join a room' });
+      }
+
+      // Verify the Firebase ID token
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      const uid = decodedToken.uid;
+
+      // Check if the user is authenticated (you can also perform additional checks here)
+      if (!uid) {
+        return res.status(401).json({ error: 'You must be logged in to join a room' });
+      }
+
       const room = await RoomModel.findOne({ roomId });
       if (!room) {
         return res.status(404).json({ error: 'Room not found' });
       }
-      
-      res.json({ message: 'Joined the room successfully' , data: room});
+
+      res.json({ message: 'Joined the room successfully', data: room });
     } catch (error) {
       res.status(500).json({ error: 'Failed to join room' });
     }
   },
-
 };
 
 function generateRandomRoomId() {
